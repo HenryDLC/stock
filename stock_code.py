e52b490c4709737aeb64fedda776663c002ecf2b
HenryDLC <henryduwork@gmail.com>
2019-02-27 21:39:29 +0800

    “参考url”

--- stock_code.py
+++ stock_code.py
@@ -1,5 +1,14 @@
+# http://baostock.com/baostock/index.php/A%E8%82%A1K%E7%BA%BF%E6%95%B0%E6%8D%AE
+# https://blog.csdn.net/wolf1132/article/details/78787466
+# https://blog.csdn.net/avenccssddnn/article/details/8628478
+# https://blog.csdn.net/llingmiao/article/details/79941066
+
+# wind https://www.jianshu.com/p/4a71f1e5db8f
+
 import tushare as ts
 
 pro = ts.pro_api('abb953ed857518585eb9ffe2e581934a213faea48fa73f288f320940')
 
-data = pro.get_hist_data()
+data = ts.get_today_all()
+
+print(data)
