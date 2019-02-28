763395271078dd281546e15bd45de8ddd483bd82
HenryDu <tiantianplayworld@gmail.com>
2019-02-28 15:32:55 +0800

    test

--- stock_code.py
+++ stock_code.py
@@ -7,8 +7,26 @@
 
 import tushare as ts
 
-pro = ts.pro_api('abb953ed857518585eb9ffe2e581934a213faea48fa73f288f320940')
+# pro = ts.pro_api('abb953ed857518585eb9ffe2e581934a213faea48fa73f288f320940')
+import time
 
-data = ts.get_today_all()
+data = ts.get_realtime_quotes('sh')
+price_one = float(data['price'])
+print(data[['code','name','price', 'time']])
+while True:
+    time.sleep(5)
+    data = ts.get_realtime_quotes('sh')
+    price_two = float(data['price'])
 
-print(data)
+
+    if price_two > price_one:
+        print("涨{:.2%}".format((price_two - price_one) / price_one))
+        price_one = price_two
+
+
+    elif price_one > price_two:
+        print("跌{:.2%}".format((price_one - price_two) / price_two))
+        price_one = price_two
+
+    elif price_two == price_one:
+        pass
