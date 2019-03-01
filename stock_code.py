9ebb265567e32bef7442095e8490ef3d0ea13769
HenryDu <tiantianplayworld@gmail.com>
2019-03-01 18:22:42 +0800

    add

--- stock_code.py
+++ stock_code.py
@@ -10,6 +10,8 @@ import tushare as ts
 # pro = ts.pro_api('abb953ed857518585eb9ffe2e581934a213faea48fa73f288f320940')
 import time
 
+price_zhang = 0
+pric_die = 0
 data = ts.get_realtime_quotes('sh')
 price_one = float(data['price'])
 print(data[['code','name','price', 'time']])
@@ -20,13 +22,16 @@ while True:
 
 
     if price_two > price_one:
-        print("涨{:.2%}".format((price_two - price_one) / price_one))
+        a = "{:.4%}".format((price_two - price_one) / price_one)
         price_one = price_two
+        price_zhang += float(a[0:6])
 
 
     elif price_one > price_two:
-        print("跌{:.2%}".format((price_one - price_two) / price_two))
+        b = "{:.4%}".format((price_one - price_two) / price_two)
         price_one = price_two
+        pric_die += float(b[0:6])
 
     elif price_two == price_one:
         pass
+    print("{:.4%}".format(price_zhang-pric_die))
